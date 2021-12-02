dd39a0a9039b9383bd287595890ba1ce9f673240
mirkoraimo <mirko.raimo@gmail.com>
2021-12-02 21:57:59 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/ui_challenge_animated_custom_drawer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ui_challenge_animated_custom_drawer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_drawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer({Key? key}) : super(key: key);
+
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  final _myDrawer = Container(color: Colors.blue);
+  final _myChild = Container(color: Colors.yellow);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Stack(
+        children: [_myDrawer, _myChild],
+      ),
+    );
+  }
+}
