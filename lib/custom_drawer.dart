6489d94b795dc3d4b4f84d68375c5ff514262f2c
mirkoraimo <mirko.raimo@gmail.com>
2021-12-02 22:24:10 +0100

    Implemented a smooth animation

--- lib/custom_drawer.dart
+++ lib/custom_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -7,26 +9,61 @@ class CustomDrawer extends StatefulWidget {
   _CustomDrawerState createState() => _CustomDrawerState();
 }
 
-class _CustomDrawerState extends State<CustomDrawer> {
-  final _myDrawer = Container(color: Colors.blue);
-  final _myChild = Container(color: Colors.yellow);
+class _CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  static const Duration _animationDurationMill = Duration(milliseconds: 250);
+
+  final Widget _myDrawer = Container(color: Colors.blue);
+  final Widget _myChild = Container(color: Colors.yellow);
+
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: _animationDurationMill,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
 
   @override
   Widget build(BuildContext context) {
     final double _width = MediaQuery.of(context).size.width;
+    final double _maxSlide = _width * 0.6;
 
     return MaterialApp(
-      home: Stack(
-        children: [
-          _myDrawer,
-          Transform(
-            transform: Matrix4.identity()
-              ..translate(_width * 0.6)
-              ..scale(0.5),
-            alignment: Alignment.centerLeft,
-            child: _myChild,
-          )
-        ],
+      home: GestureDetector(
+        onTap: toggle,
+        child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, _) {
+              double slide = _maxSlide * _animationController.value;
+              double scale = 1 - (_animationController.value * 0.3);
+
+              return Stack(
+                children: [
+                  _myDrawer,
+                  Transform(
+                    transform: Matrix4.identity()
+                      ..translate(slide)
+                      ..scale(scale),
+                    alignment: Alignment.centerLeft,
+                    child: _myChild,
+                  )
+                ],
+              );
+            }),
       ),
     );
   }
